d5fc1b76b6349ef577aa050846990f5c93939dc4
Alexander <a.alexanderpavon@gmail.com>
2025-08-11 05:40:03 -0500

    Arreglo de reportes 5

--- src/pages/ReportsPage.js
+++ src/pages/ReportsPage.js
@@ -31,7 +31,7 @@ export default function ReportsPage() {
       if (res.ok && res.data?.url) {
         const fullUrl = res.data.url.startsWith("http")
           ? res.data.url
-          : `${API_ROOT}${res.data.url}`;
+          : `${API_URL}${res.data.url}`;
         setUrlReporte(fullUrl);
         setMensaje(res.data.message || "Reporte generado correctamente.");
       } else {
